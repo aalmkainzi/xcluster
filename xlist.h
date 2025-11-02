cfb855e5a4cedba075eb74817bd7481ea8af0648
Abdulmalek Almkainzi <aalmkainzi@gmail.com>
2025-11-02 15:30:46 +0300

    sixthirty

---
+++ xlist.h
@@ -0,0 +1,146 @@
+/*
+XList stands for "Exploding Array",
+when deleting, it splits the the hole into 2 arrays,
+in order to maintain pointer/iterator stability of other elements.
+
+
+
+when inserting, it looks into stack of non_full_buckets, if empty, alloc new bucket.
+
+when deleting, split into 2 arrays, left one is full from the start, right one will keep the old cap + left_arr_size
+
+but how will iterators be stable? and what about it_next()?
+
+
+
+*/
+
+#include <stddef.h>
+#include <stdint.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if !defined(XLIST_T) || !defined(XLIST_NAME) || !defined(XLIST_SENTINEL) || !defined(XLIST_IS_SENTINEL) || !defined(XLIST_PTR_FIELD)
+    #error "Must define XLIST_T, XLIST_NAME, XLIST_SENTINEL, and XLIST_IS_SENTINEL"
+#endif
+
+typedef struct XListBucket
+{
+    // maybe we do add prev ptr here. not for linked list traversal, but to say they are actually connected in memory, so if this bucket is about to be deleted, the prev one can just extend its cap. the prev field can be NULL
+    // issue is, if prev is already deleted....
+    // this idea is a wash, just have a bucket reserve so we can reuse its buffers
+    
+    
+    size_t not_full_index;
+    XLIST_T *elms;
+    size_t count;
+    size_t cap;
+} XListBucket;
+
+typedef struct XList
+{
+    XListBucket **not_full_buckets;
+    size_t nfb_count;
+    size_t nfb_cap;
+    
+    XListBucket *buckets;
+    size_t b_count;
+    size_t b_cap;
+    
+    size_t count;
+} XList;
+
+typedef struct XListIterator
+{
+    XLIST_T *ptr;
+    //size_t id; // something to do with index+bucket?
+    // maybe we can store a second buffer at the bucket?
+    // or the one-past-last elm stores some data about owning bucket? (might be the way)
+    // issue is, without a ptr to the bucket, we dont know where the end is (maybe sentinel value?)
+    
+    // new idea:
+    //void *allocation; //key1
+    //size_t idx_within_allocation; //key2
+    // these two fields will be used as key into some map
+    
+    // or maybe use sentinel value to mark end of array (-1 for ints for example)
+    // the address of the sentinel value will be used as a key in some map to get the owning bucket. (THIS MIGHT BE THE WAY!)
+    
+    // new idea: the main list structure holds array of all buckets, just check if ptr >= begin and < end of which bucket
+    
+    // maybe require the sentinel have a ptr field, which we can reuse to make it a ptr to the bucket
+    // so this means: requires sentinel, but with one field we can customize
+} XListIterator;
+
+
+void xlist_init(XList *ls)
+{
+    memset(ls, 0, sizeof(XList));
+    ls->b_cap = 16;
+    ls->b_count = 0;
+    ls->buckets = malloc(sizeof(XListBucket) * ls->b_cap);
+    
+    ls->nfb_cap = 16;
+    ls->nfb_count = 0;
+    ls->not_full_buckets = malloc(sizeof(XListBucket*) * ls->nfb_cap);
+}
+
+#define XLIST_MAYBE_GROW(ptr, cap_ptr, count) \
+do \
+{ \
+    size_t *_cap = (cap_ptr); \
+    const size_t _count = (count); \
+    if(_count >= *_cap) \
+    { \
+        *_cap *= 2; \
+        ptr = realloc(ptr, *_cap * sizeof(*(ptr))); \
+    } \
+} while(0)
+
+XLIST_T *xlist_put_uninit(XList *ls)
+{
+    if(ls->nfb_count != 0)
+    {
+        ls->count++;
+        
+        XListBucket *bucket = ls->not_full_buckets[ls->nfb_count - 1];
+        bucket->count++;
+        
+        if(bucket->count == bucket->cap)
+        {
+            bucket->not_full_index = -1;
+            ls->nfb_count -= 1;
+        }
+        
+        return &bucket->elms[bucket->count - 1];
+    }
+    
+    XLIST_MAYBE_GROW(ls->buckets, &ls->b_cap, ls->b_count);
+    XLIST_MAYBE_GROW(ls->not_full_buckets, &ls->nfb_cap, ls->nfb_count);
+    
+    XListBucket *new_bucket = &ls->buckets[ls->b_count++];
+    
+    memset(new_bucket, 0, sizeof(XListBucket));
+    new_bucket->not_full_index = ls->nfb_count++;
+    new_bucket->cap = 64;
+    new_bucket->elms = malloc(sizeof(XLIST_T) * new_bucket->cap);
+    new_bucket->count = 1;
+    return &new_bucket->elms[0];
+}
+
+XLIST_T *xlist_del(XList *ls, XLIST_T *elm)
+{
+    XLIST_T *end = elm;
+    while(!XLIST_IS_SENTINEL(end))
+    {
+        end++;
+    }
+    
+}
+
+
+
+
+
+
